78e3115351c23f4b97c57023f9648025fcf6c354
dziedzickajolanta <125757081+dziedzickajolanta@users.noreply.github.com>
2023-04-02 19:36:04 +0200

    Update main.py

--- NATO FONETIC/main.py
+++ NATO FONETIC/main.py
@@ -4,8 +4,14 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_words = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
+def function():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [phonetic_words[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in alphabet.")
+        function()
+    else:
+        print(result)
 
-letters_of_word = [n for n in word]
-result = [phonetic_words[letter] for letter in word]
-print(result)
+function()
